e54fb6d4a900a3ad9aafa158102a773477068426
Ashish17496 <civilmishra11@gmail.com>
2022-12-28 23:12:07 +0530

    Change in demo class

--- src/learn/jdbc/Demo.java
+++ src/learn/jdbc/Demo.java
@@ -4,8 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Scanner;
-
+/**
+ * This is Demo Application
+ * */
 public class Demo {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
